8c6d9eb31467dbb9ed040958b14f24466fa6c57a
Marcel <marcellinuscnnaudi@gmail.com>
2022-06-13 06:11:50 +0100

    An array of strings smashed with use strict

---
+++ main.js
@@ -0,0 +1,12 @@
+// Smash Words
+function smash (words) {
+    "use strict";
+    var smashed = '';
+    for(var i = 0; i<words.length; i++) {
+      smashed += words[i];
+      if(i!=words.length-1) {
+        smashed += ' ';
+      }
+    }
+    return smashed;
+};
